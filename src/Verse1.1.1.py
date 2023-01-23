da6815c21b17bdb8a6e412580b3e02f6839e2b39
Maya <skorodinamaya@mail.ru>
2023-01-23 15:43:50 +0300

    Update Verse1.1.1.py

--- src/Verse1.1.1.py
+++ src/Verse1.1.1.py
@@ -171,20 +171,20 @@ class OverlayImage:
         return self.x, self.y, self.matrix
 
 
-WIDTH, HEIGHT = 800, 500
-QUALITY = 200
-NAME = 'src/image/dog.png'
+# WIDTH, HEIGHT = 800, 500
+# QUALITY = 200
+# NAME = 'src/image/dog.png'
 
-MAIN_IMAGE = MainImage(WIDTH, HEIGHT)
+# MAIN_IMAGE = MainImage(WIDTH, HEIGHT)
 
-for t in range(QUALITY):
-    # all changes. check that everyone is here
-    OVER_IMAGE = OverlayImage(NAME)
-    OVER_IMAGE.crop()
-    OVER_IMAGE.edit_random(WIDTH, HEIGHT)
-    OVER_IMAGE.create_matrix()
-    OVER_IMAGE.save_rez()
-    # all changes. check that everyone is here
-    datas = OVER_IMAGE.get_data()
-    MAIN_IMAGE.add_images(datas)
-    MAIN_IMAGE.save_rez()
+# for t in range(QUALITY):
+#     # all changes. check that everyone is here
+#     OVER_IMAGE = OverlayImage(NAME)
+#     OVER_IMAGE.crop()
+#     OVER_IMAGE.edit_random(WIDTH, HEIGHT)
+#     OVER_IMAGE.create_matrix()
+#     OVER_IMAGE.save_rez()
+#     # all changes. check that everyone is here
+#     datas = OVER_IMAGE.get_data()
+#     MAIN_IMAGE.add_images(datas)
+#     MAIN_IMAGE.save_rez()
