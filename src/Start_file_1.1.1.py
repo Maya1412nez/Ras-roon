cbe49c2853e02934193f56b17d2d9940b920ed2e
Maya <skorodinamaya@mail.ru>
2023-01-23 15:47:03 +0300

    Create Start_file_1.1.1.py

---
+++ src/Start_file_1.1.1.py
@@ -0,0 +1,19 @@
+from Verse1_1_1 import MainImage, OverlayImage
+
+WIDTH, HEIGHT = 800, 500
+QUALITY = 20
+NAME = 'src/image/dog.png'
+
+MAIN_IMAGE = MainImage(WIDTH, HEIGHT)
+
+for t in range(QUALITY):
+    # all changes. check that everyone is here
+    OVER_IMAGE = OverlayImage(NAME)
+    OVER_IMAGE.crop()
+    OVER_IMAGE.edit_random(WIDTH, HEIGHT)
+    OVER_IMAGE.create_matrix()
+    OVER_IMAGE.save_rez()
+    # all changes. check that everyone is here
+    datas = OVER_IMAGE.get_data()
+    MAIN_IMAGE.add_images(datas)
+    MAIN_IMAGE.save_rez()
