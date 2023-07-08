a02bea1abdfc8a2d721b1068f9d4e5328ae27bbb
Maya <skorodinamaya@mail.ru>
2023-07-08 16:10:06 +0300

    grafics

--- src/versions/2.py
+++ src/versions/2.py
@@ -3,9 +3,10 @@ from matplotlib import pyplot as plt
 import collections
 
 
-plt.title("Зависимость массы эпоксидной смолы от времени") 
-plt.xlabel("Время (сутки)") 
-plt.ylabel("Масса образца") 
+
+plt.title("Изменение массы образцов из эпоксидной смолы во времени") 
+plt.xlabel("Время, сутки") 
+plt.ylabel("Масса образца, гр") 
 
 
 
@@ -16,18 +17,26 @@ plt.ylabel("Масса образца")
 #     plt.plot(x[i:i+2], y[i:i+2], 'ro-')
 # plt.show()
 def get_data(name):
+    days = [1, 2, 3, 4]
     if name == 'Ацетон':
-        return([0, 1, 2], [0.661, 0.708, 0.728], 0, 0.03)
+        notes = [(4 - 0.4, 0.718 + 0.07, 'Отломился кусок')]
+        return(days, [0.661, 0.708, 0.728, 0.718], 0, 0.03, notes)
     if name == 'KOH':
-        return ([0, 1, 2], [0.733, 0.753, 0.762], 0, 0.03)
+        notes = []
+        return (days, [0.733, 0.753, 0.762, 0.769], 0, 0.03, notes)
     if name == 'H2SO4 конц.':
-        return([0, 1], [0.887, 1.551], 0.1, 0)
+        notes = [(1 + 0.05, 1.551, 'Растворился')]
+        return([0, 1], [0.887, 1.551], 0.1, 0, notes)
     if name == 'Морская вода':
-        return([0, 1, 2], [0.593, 0.603, 0.613], 0, 0.022)
+        notes = []
+        return(days, [0.593, 0.603, 0.613, 0.629], 0, 0.022, notes)
 
 list_of_ress = ['Ацетон', 'KOH', 'H2SO4 конц.', 'Морская вода']
 for name in list_of_ress:
-    x1, y1, move_x, move_y = get_data(name)
+    x1, y1, move_x, move_y, notes = get_data(name)
+    for el in notes:
+        plt.text(el[0], el[1], el[2])
+        print(el[2])
     plt.plot(x1,y1) 
     plt.text(x1[0] + move_x, y1[0] + move_y, name)
     plt.plot(x1,y1, 'bo') 
--- src/versions/import numpy as np.py
+++ src/versions/import numpy as np.py
@@ -3,9 +3,9 @@ from matplotlib import pyplot as plt
 import collections
 
 
-plt.title("Зависимость ") 
-plt.xlabel("количество фигур") 
-plt.ylabel("коэфициент качества") 
+plt.title("Изменение плотности от количества микросфер") 
+plt.xlabel("количество микросфер (гр)") 
+plt.ylabel("плотность композита") 
 # plt.plot(0, 1.05, 'ro') # 1:1
 # plt.plot(10.34, 0.918, 'ro-') # 2:3
 # plt.plot(14.46, 0.867, 'ro') #1:2
@@ -13,10 +13,11 @@ plt.ylabel("коэфициент качества")
 
 
 
-x, y = [0, 10.34, 14.46, 28.28], [1.05, 0.918, 0.867, 0.608]
+x, y = [0, 10.34, 14.46], [1.05, 0.918, 0.867]
 # fig, ax = plt.subplots()  # Create a figure containing a single axes.
 # ax.plot([0, 10.34, 14.46], [1.05, 0.918, 0.867])  # Plot some data on the axes.
-for i in range(0, len(x)):
-    plt.plot(x[i:i+2], y[i:i+2], 'ro-')
+plt.plot(x, y)
+plt.plot(x, y, 'ro')
+# plt.text(x[-1] - 0.2, y[-1] + 0.01, 'ПВХ')
 plt.show()
 
